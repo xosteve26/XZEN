736338bb59552e363aa485f5ff8b5f171ebf8ae5
Steve <stevedood76@gmail.com>
2021-02-24 20:22:46 +0530

    Looped Through Items As Cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import Homescreen from './screens/HomeScreen'
 
 const App =() => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className='py-3'>
+          <Container>
+            <h1 className="text-center">Welcome to XZen</h1>
+            <Homescreen />
+          </Container>
+        
+      </main>
+      <Footer />
+      
       
     </>
   );
